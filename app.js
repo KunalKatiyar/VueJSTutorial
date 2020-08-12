dbbe6c6b561e2197717c936b6c3f90ce13e5de8f
KunalKatiyar <katiyarkunal2011@gmail.com>
2020-08-12 08:26:26 +0530

    Initial Commit (Till Events)

---
+++ app.js
@@ -0,0 +1,27 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        name: 'Kunal',
+        job: 'Student',
+        website: "https://kunalkatiyar.github.io",
+        websiteTag: '<a href="https://kunalkatiyar.github.io">Kunal Katiyar Website</a>',
+        age: 25,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        greet: function(time){
+            return 'Good ' + time + " " + this.name;
+        },
+        add: function(inc){
+            this.age+=inc;
+        },
+        subtract: function (dec) {
+            this.age-=dec;
+        },
+        updateXY: function (event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
+    }
+});
