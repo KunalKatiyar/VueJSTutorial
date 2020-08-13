a12280fba5e2bcb75f70bbe6f5c125a23bf8021d
KunalKatiyar <katiyarkunal2011@gmail.com>
2020-08-13 19:54:00 +0530

    Simple Punchbag Game

--- app.js
+++ app.js
@@ -1,64 +1,22 @@
 new Vue({
     el: '#vue-app',
     data: {
-        // name: '',
-        // job: 'Student',
-        // website: "https://kunalkatiyar.github.io",
-        // websiteTag: '<a href="https://kunalkatiyar.github.io">Kunal Katiyar Website</a>',
-        // age: 20,
-        // a: 0,
-        // b: 0
-        // x: 0,
-        // y: 0
-        // available: false,
-        // nearby: false
-        // error: false,
-        // success: false
-        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-        ninjas: [
-            { name: 'Ryu', age: 25 },
-            { name: 'Yoshi', age: 35 },
-            { name: 'Ken', age: 55 }
-        ]
+        health: 100,
+        ended: false
     },
     methods: {
-        greet: function(time){
-            return 'Good ' + time + " " + this.name;
+        punch: function(){
+            this.health -= 10;
+            if (this.health <= 0){
+                this.ended = true;
+            }
         },
-        add: function(inc){
-            this.age+=inc;
-        },
-        subtract: function (dec) {
-            this.age-=dec;
-        },
-        updateXY: function (event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click: function () {
-            alert('You clicked me')
-        },
-        logName: function(){
-            console.log('You entered your name');
-        },
-        logAge: function(){
-            console.log('You entered your age');
+        restart: function () {
+            this.health = 100;
+            this.ended = false;
         }
     },
     computed: {
-        addToA: function () {
-            console.log('addToA');
-            return this.a + this.age;
-        },
-        addToB: function () {
-            console.log('addToB');
-            return this.b + this.age;
-        },
-        compClasses: function(){
-            return {
-                available: this.available,
-                nearby: this.nearby
-            }
-        }
+        
     }
 });
